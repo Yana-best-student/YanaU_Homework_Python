2b6a3a61035f422b51a7eb810b220ebebf28322c
Yana-best-student <yan16752009@yandex.ru>
2025-10-14 08:53:58 +0500

    изменения в файл практика

--- lesson_02/practika2.py
+++ lesson_02/practika2.py
@@ -1,171 +1,171 @@
-#Задание 1
+Задание 1
 
-#  employee_list = ["John Snow", "Piter Pen", "Drakula", "IvanIV", "Malifisenta", "Juilet", "Moana"]
+ employee_list = ["John Snow", "Piter Pen", "Drakula", "IvanIV", "Malifisenta", "Juilet", "Moana"]
 
-# print(employee_list[1] + ", " + employee_list[-2])
+print(employee_list[1] + ", " + employee_list[-2])
 
-# DOMASHKA
-# lst = [ '🍇', '🍑', '🍐', '🍊', '🍌', '🍎']
+DOMASHKA
+lst = [ '🍇', '🍑', '🍐', '🍊', '🍌', '🍎']
 
-# print(lst[1])
-# print(lst[-1])
+print(lst[1])
+print(lst[-1])
 
-#Задание 2
+Задание 2
 
-#  def dev_by_three(x):
-#     return "Да" if x % 3 == 0 else "Нет"
+ def dev_by_three(x):
+    return "Да" if x % 3 == 0 else "Нет"
 
-# num = int(input("Введите число: "))
-# result = dev_by_three(num)
-# print(f"Делиться ли на три {num}? - {result}")
+num = int(input("Введите число: "))
+result = dev_by_three(num)
+print(f"Делиться ли на три {num}? - {result}")
 
-# DOMASHKA
-# def is_year_leap(x):
-#     return "True" if x % 4 == 0 else "False"
+DOMASHKA
+def is_year_leap(x):
+    return "True" if x % 4 == 0 else "False"
 
-# num = int(input("Введите год: "))
-# result = is_year_leap(num)
-# print(f"Год {num} : {result}")
-
-
-#Задание 3
-
-# import math
+num = int(input("Введите год: "))
+result = is_year_leap(num)
+print(f"Год {num} : {result}")
 
 
-# def min_boxes(thing):
-#    return math.ceil(thing / 5)
+Задание 3
 
-# num_things = int(input("Введите количество предметов: "))
-# print(f"Минимальное количество коробок: {min_boxes(num_things)}")
-
-# DOMASHKA
-# import math
+import math
 
 
-# def square(side):
-#    return math.ceil(side * side)
+def min_boxes(thing):
+   return math.ceil(thing / 5)
 
-# num_side = float(input("Введите сторону квадрата: "))
-# print(f"Площадь квадрата равна: {square(num_side)}")
+num_things = int(input("Введите количество предметов: "))
+print(f"Минимальное количество коробок: {min_boxes(num_things)}")
 
-#Задание 4
-
-
-# n = int(input("Введите число:"))
+DOMASHKA
+import math
 
 
-# def check_divisibility(n):
-#     for i in range(1, n + 1):
-#         if i % 4 == 0:
-#             print(f"{i} - Делится и на 2, и на 4")
-#         elif i % 2 == 0:
-#             print(f"{i} - Делится на 2, но не на 4")
-#         else:
-#             print(i)
+def square(side):
+   return math.ceil(side * side)
+
+num_side = float(input("Введите сторону квадрата: "))
+print(f"Площадь квадрата равна: {square(num_side)}")
+
+Задание 4
 
 
-# check_divisibility(n)
+n = int(input("Введите число:"))
 
-# DOMASHKA
-# n = int(input("Введите число:"))
 
-# def fizz_buzz(n):
-#     for n in range(1, n + 1):
-#         if n % 5 == 0 and n % 3 == 0:
-#             print("FizzBuzz")
-#         elif n % 3 == 0:
-#             print("Fizz")
-#         elif n % 5 == 0:
-#             print("Buzz")      
+def check_divisibility(n):
+    for i in range(1, n + 1):
+        if i % 4 == 0:
+            print(f"{i} - Делится и на 2, и на 4")
+        elif i % 2 == 0:
+            print(f"{i} - Делится на 2, но не на 4")
+        else:
+            print(i)
+
+
+check_divisibility(n)
+
+DOMASHKA
+n = int(input("Введите число:"))
+
+def fizz_buzz(n):
+    for n in range(1, n + 1):
+        if n % 5 == 0 and n % 3 == 0:
+            print("FizzBuzz")
+        elif n % 3 == 0:
+            print("Fizz")
+        elif n % 5 == 0:
+            print("Buzz")      
         
 
-#         else:
-#             print(n)     
+        else:
+            print(n)     
         
-# fizz_buzz(n)
+fizz_buzz(n)
 
-#Задание 5
+Задание 5
 
-# def quarter_of_year(n):
+def quarter_of_year(n):
     
-#     if 1 <= n <= 3:
-#        return "I квартал"
-#     elif 4 <= n <= 6:
-#        return "II квартал"
-#     elif 7 <= n <= 9:
-#        return "III квартал"
-#     elif 10 <= n <= 12:
-#        return "IV квартал"
-#     else:
-#        return "Неверный номер месяца"
+    if 1 <= n <= 3:
+       return "I квартал"
+    elif 4 <= n <= 6:
+       return "II квартал"
+    elif 7 <= n <= 9:
+       return "III квартал"
+    elif 10 <= n <= 12:
+       return "IV квартал"
+    else:
+       return "Неверный номер месяца"
     
-# try:
-#    n = int(input("Введите номер месяца (1-12):"))
-#    print(quarter_of_year(n)) 
-# except ValueError:
-#     print("Пожалуйста, введите целое число от 1 до 12.")
+try:
+   n = int(input("Введите номер месяца (1-12):"))
+   print(quarter_of_year(n)) 
+except ValueError:
+    print("Пожалуйста, введите целое число от 1 до 12.")
 
-# DOMASHKA
-# def monthon_to_season(n):
+DOMASHKA
+def monthon_to_season(n):
     
-#     if 1 <= n <= 2:
-#        return "Зима"
-#     elif 3 <= n <= 5:
-#        return "Весна"
-#     elif 6 <= n <= 8:
-#        return "Лето"
-#     elif 9 <= n <= 11:
-#        return "Осень"
-#     elif n == 12:
-#        return "Зима"
-#     else:
-#        return "Неверный номер месяца"
+    if 1 <= n <= 2:
+       return "Зима"
+    elif 3 <= n <= 5:
+       return "Весна"
+    elif 6 <= n <= 8:
+       return "Лето"
+    elif 9 <= n <= 11:
+       return "Осень"
+    elif n == 12:
+       return "Зима"
+    else:
+       return "Неверный номер месяца"
     
-# try:
-#    n = int(input("Введите номер месяца (1-12):"))
-#    print(monthon_to_season(n)) 
-# except ValueError:
-#     print("Пожалуйста, введите целое число от 1 до 12.")
+try:
+   n = int(input("Введите номер месяца (1-12):"))
+   print(monthon_to_season(n)) 
+except ValueError:
+    print("Пожалуйста, введите целое число от 1 до 12.")
         
 
 
-# Задание  6
+Задание  6
 
-# lst = [17, 34, 9, 21, 13, 48, 24, 7, 81, 29, 16, 12, 42]
+lst = [17, 34, 9, 21, 13, 48, 24, 7, 81, 29, 16, 12, 42]
 
-# for n in lst:
-# 	if (n % 3 == 0) and (n > 15):
-# 		print(n)
+for n in lst:
+	if (n % 3 == 0) and (n > 15):
+		print(n)
 		
-# DOMASHKA
+DOMASHKA
 
-# lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
+lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
 
-# for n in lst:
-# 	if (n % 3 == 0) and (n < 30):
-# 		print(n)
-
-
-# Задание 7
+for n in lst:
+	if (n % 3 == 0) and (n < 30):
+		print(n)
 
 
-# list = list(range(25, 0, -5))
-# print(list)
+Задание 7
 
-# DOMASHKA
 
-# list = list(range(18, 0, -4))
-# print(list)
+list = list(range(25, 0, -5))
+print(list)
 
-# Задание 8
+DOMASHKA
 
-# var_1 = 50
-# var_2 = 5
+list = list(range(18, 0, -4))
+print(list)
 
-# var_1, var_2 = var_2, var_1
+Задание 8
 
-# print(f"var_1= {var_1}, var_2= {var_2}")
+var_1 = 50
+var_2 = 5
+
+var_1, var_2 = var_2, var_1
+
+print(f"var_1= {var_1}, var_2= {var_2}")
  
 
 
