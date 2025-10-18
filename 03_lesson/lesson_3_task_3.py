0c4d49e2fcd21b0c8bbab4caf79c204daf738b6f
Yana-best-student <yan16752009@yandex.ru>
2025-10-19 00:22:10 +0500

    форматирование

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -6,8 +6,11 @@ from_address = Address("617400", "Москва", "Ленина", "10", "75")
 
 mailing = Mailing("TRK476990", from_address, to_address, "150")
 print(
-    f"Почтовое отправление номер: {mailing.trak} из {mailing.from_address.postal_code}, {mailing.from_address.city}, "
-    f"{mailing.from_address.street} {mailing.from_address.house} - {mailing.from_address.apartment} в "
-    f"{mailing.to_address.postal_code}, {mailing.to_address.city}, {mailing.to_address.street} "
-    f"{mailing.to_address.house} - {mailing.to_address.apartment}. Стоимостью {mailing.cost} рублей."
+    f"Отправление номер: {mailing.trak} из {mailing.from_address.postal_code}, "
+    f"{mailing.from_address.city}, {mailing.from_address.street}"
+    f" {mailing.from_address.house} - {mailing.from_address.apartment} в "
+    f"{mailing.to_address.postal_code}, {mailing.to_address.city}, "
+    f"{mailing.to_address.street} "
+    f"{mailing.to_address.house} - {mailing.to_address.apartment}."
+    f" Стоимостью {mailing.cost} рублей."
 )
--- 03_lesson/mailing.py
+++ 03_lesson/mailing.py
@@ -1,11 +1,12 @@
 from address import Address
 
+
 class Mailing:
     def __init__(self, track: str, from_address: Address, to_address: Address,  cost: float):
         self.trak = track
         self.to_address = to_address
         self.from_address = from_address
         self.cost = cost
-        
+
     def __str__(self):
         return f"Почтовое отправление номер {self.trak} из {self.from_address} в {self.to_address}. Cтоимостью {self.cost} рублей"
