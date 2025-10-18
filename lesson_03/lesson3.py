d9364475cd673f8deb19c5876de7dab440497d0b
Yana-best-student <yan16752009@yandex.ru>
2025-10-18 16:37:38 +0500

    practika po uroky 3

---
+++ lesson_03/lesson3.py
@@ -0,0 +1,16 @@
+from user import User
+from card import Card
+
+alex = User("Alex")
+
+
+alex.sayName()
+alex.setAge(33)
+alex.sayAge()
+
+card = Card("1111 1234 5678 8765", "11/28", "Alex F")
+
+alex.addCard(card)
+alex.getCard().pay(1000)
+
+
---
+++ lesson_03/practik3/ex1.py
@@ -0,0 +1,12 @@
+from product import Product
+
+my_product = Product("iPhone", 120000)
+
+print(my_product.get_name())
+print(my_product.get_price())
+print(my_product.get_product_info())
+
+
+
+
+
