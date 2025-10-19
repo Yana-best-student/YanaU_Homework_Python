82dac827bfabee8b4f470037b022ee7a59bdfa23
Yana-best-student <yan16752009@yandex.ru>
2025-10-19 11:33:44 +0500

    внесение изменений

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -6,7 +6,8 @@ from_address = Address("617400", "Москва", "Ленина", "10", "75")
 
 mailing = Mailing("TRK476990", from_address, to_address, "150")
 print(
-    f"Отправление номер: {mailing.trak} из {mailing.from_address.postal_code}, "
+    f"Отправление номер: {mailing.trak} из "
+    f"{mailing.from_address.postal_code}, "
     f"{mailing.from_address.city}, {mailing.from_address.street}"
     f" {mailing.from_address.house} - {mailing.from_address.apartment} в "
     f"{mailing.to_address.postal_code}, {mailing.to_address.city}, "
