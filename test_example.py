ca87e275753d21db027d694092ea4c9eee8c91bc
Yana-best-student <yan16752009@yandex.ru>
2025-10-26 19:49:11 +0500

    практика по 4 уроку

---
+++ test_example.py
@@ -0,0 +1,27 @@
+# def test_addition():
+#     assert 1 + 1 == 2
+
+import pytest
+from string_processor import StringProcessor
+
+
+@pytest.mark.parametrize("input_text, expected_output",
+                         [
+                             ("yana", "Yana."),
+                             ("яна", "Яна."),
+                             ("hello, Yana", "Hello, Yana."),
+
+                         ],
+                         )
+def test_process_positive(input_text,
+                          expected_output):
+    processor = StringProcessor()
+    assert processor.process(input_text) == expected_output
+
+@pytest.mark.parametrize(
+    "input_text, expected_output",
+    [("", "."), ("    ", "    .")],
+)
+def test_process_negative(input_text, expected_output):
+    processor = StringProcessor()
+    assert processor.process(input_text) == expected_output    
