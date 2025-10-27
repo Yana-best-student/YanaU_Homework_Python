37bc4a6d1f653e10b17fe3bdcc5048821c3e9917
Yana-best-student <yan16752009@yandex.ru>
2025-10-27 19:36:03 +0500

    домашнее задание 4

--- test_example.py
+++ test_example.py
@@ -5,21 +5,17 @@ import pytest
 from string_processor import StringProcessor
 
 
-@pytest.mark.parametrize("input_text, expected_output",
-                         [
-                             ("yana", "Yana."),
-                             ("яна", "Яна."),
-                             ("hello, Yana", "Hello, Yana."),
-
-                         ],
-                         )
-def test_process_positive(input_text,
-                          expected_output):
+@pytest.mark.parametrize("input_text, expected_output", [
+    ("yana", "Yana."),
+    ("яна", "Яна."),
+    ("hello, Yana", "Hello, Yana."),
+],
+)
+def test_process_positive(input_text, expected_output):
     processor = StringProcessor()
     assert processor.process(input_text) == expected_output
 
-@pytest.mark.parametrize(
-    "input_text, expected_output",
+@pytest.mark.parametrize("input_text, expected_output",
     [("", "."), ("    ", "    .")],
 )
 def test_process_negative(input_text, expected_output):
