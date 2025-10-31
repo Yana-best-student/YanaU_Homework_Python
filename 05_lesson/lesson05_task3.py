e7a7d1bb290fb55894372ede0f124d8524fd09ab
Yana-best-student <yan16752009@yandex.ru>
2025-10-31 23:23:43 +0500

    домашка по уроку 5

---
+++ 05_lesson/lesson05_task3.py
@@ -0,0 +1,22 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Firefox(
+    service=FirefoxService(GeckoDriverManager().install()))
+driver.get("http://the-internet.herokuapp.com/inputs")
+
+number_input = driver.find_element(By.CSS_SELECTOR, "input[type='number']")
+sleep(5)
+number_input.send_keys("Sky")
+sleep(5)
+number_input.clear()
+sleep(5)
+number_input = driver.find_element(By.CSS_SELECTOR, "input[type='number']")
+sleep(5)
+number_input.send_keys("Pro")
+sleep(5)
+driver.quit()
