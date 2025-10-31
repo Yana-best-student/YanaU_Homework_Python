4b3aaa46f459e4fc6111128be67756f0021ad4b2
Yana-best-student <yan16752009@yandex.ru>
2025-10-31 13:11:59 +0500

    сохранение файлов

--- lesson_05/practica.py
+++ lesson_05/practica.py
@@ -5,7 +5,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=ChromeService(
+    ChromeDriverManager().install()))
 
 # Вывести заголовок страницы
 driver.get("https://www.example.com")
