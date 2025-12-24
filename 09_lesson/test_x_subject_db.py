00a26061870a738b2bd7b1197f3f76949bbed522
Yana-best-student <yan16752009@yandex.ru>
2025-12-24 17:28:04 +0500

    решение задания

--- 09_lesson/test_x_subject_db.py
+++ 09_lesson/test_x_subject_db.py
@@ -1,8 +1,20 @@
+import pytest
 from sqlalchemy import create_engine, text
 from sqlalchemy import inspect
 
 db_connection_string = "postgresql://postgres:0511@localhost/QA"
 db = create_engine(db_connection_string)
+
+
+@pytest.fixture
+def db_connection():
+    engine = create_engine(db_connection_string)
+    connection = engine.connect()
+
+    transaction = connection.begin()
+    yield connection
+    transaction.rollback()
+    connection.close()
 
 
 def test_db_connection():
@@ -11,38 +23,53 @@ def test_db_connection():
     assert 'subject' in names
 
 
-def test_insert_new_subject_title():
-    db = create_engine(db_connection_string)
-    # создаем новый предмет с номером и названием
+def test_insert_new_subject_title(db_connection):
+    transaction = db_connection.begin()
     insert_sql = text(
-        "INSERT INTO subject(\"subject_id\", \"subject_title\") values(:id, :title)")
-    my_params = {
-        'id': 17,
-        'title': 'Physical Education'
-    }
-    with db.begin() as connection:
-        connection.execute(insert_sql, my_params)
-        select_sql = text("SELECT * FROM subject WHERE subject_id = :id AND subject_title = :title")
-        select_result = connection.execute(select_sql, my_params)
-        row = select_result.fetchone()
-        assert row['subject_id'] == 17
-        assert row['subject_title'] == 'Physical Education'
-        delete_sql = text("DELETE FROM subject WHERE subject_id = :id AND subject_title = :title") # удаляем  новый предмет
-        connection.execute(delete_sql, my_params)
+        "INSERT INTO subject(subject_id, subject_title) VALUES(:id, :title)")
+    my_params = {'id': 17, 'title': 'Physical Education'}
+    db_connection.execute(insert_sql, my_params)
+    transaction.commit()
+    select_sql = text(
+        "SELECT * FROM subject WHERE subject_id = :id AND subject_title = :title")
+    select_result = db_connection.execute(select_sql, my_params)
+    row = select_result.fetchone()
+    assert row['subject_id'] == 17
+    assert row['subject_title'] == 'Physical Education'
 
 
-def test_update_new_subject_title():
+def test_update_new_subject_title(db_connection):
+    transaction = db_connection.begin()
+    insert_sql = text(
+        "INSERT INTO subject(subject_id, subject_title) VALUES(:id, :title)")
+    my_params = {'id': 17, 'title': 'Physical Education'}
+    db_connection.execute(insert_sql, my_params)
+    transaction.commit()
+    select_sql = text(
+        "SELECT * FROM subject WHERE subject_id = :id AND subject_title = :title")
+    select_result = db_connection.execute(select_sql, my_params)
+    row = select_result.fetchone()
+    assert row['subject_id'] == 17
+    assert row['subject_title'] == 'Physical Education'
+    # # меняем название предмета
+    update_sql = text(
+        "UPDATE subject SET subject_title = :new_title WHERE subject_id = :id")
+    update_params = {'id': 17, 'new_title': 'Art'}
+    db_connection.execute(update_sql, update_params)
+    transaction.commit()
+    select_sql = text("SELECT * FROM subject WHERE subject_id = :id")
+    select_result = db_connection.execute(select_sql, {'id': 17})
+    row = select_result.fetchone()
+    assert row['subject_title'] == 'Art'
 
-    # создаем новый предмет с номером и названием
-    sql = text(
-        "INSERT INTO subject (subject_id, subject_title) values (:new_id, :new_title)")
-    rows = (sql, {'new_id': 17, 'new_title': 'Physical Education'})
 
-    # меняем название предмета
-    sql = text(
-        "UPDATE subject SET (subject_id, subject_title) values (:new_id, :new_title)")
-    rows = (sql, {'new_id': 17, 'new_title': 'Art'})
-
-    # удаляем за собой новый предмет
-    sql = text("DELETE FROM subject WHERE subject_id = :new_id")
-    rows = db.execute(sql, {"new_id": 17})
+def test_delete_subject_title(db_connection):
+    initial_count = db_connection.execute(
+        text("SELECT COUNT(*) FROM subject")).scalar()
+    db_connection.execute(text("INSERT INTO subject (subject_id, subject_title) VALUES (:new_id, :new_title)"), {
+                          'new_id': 17, 'new_title': 'Physical Education'})
+    db_connection.execute(
+        text("DELETE FROM subject WHERE subject_id = :new_id"), {"new_id": 17})
+    final_count = db_connection.execute(
+        text("SELECT COUNT(*) FROM subject")).scalar()
+    assert initial_count == final_count
